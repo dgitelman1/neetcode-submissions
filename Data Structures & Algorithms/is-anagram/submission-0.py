9748aed5763dd1f8ea32df2c95c2c5e68ebe8f09
Daniel G <92356476+dgitelman1@users.noreply.github.com>
2026-08-31 13:59:43 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        letters = [0] * 26
+        for c in s:
+            letters[ord(c)-ord('a')]+=1
+        for c in t:
+            letters[ord(c)-ord('a')]-=1
+        return all(x == 0 for x in letters)
