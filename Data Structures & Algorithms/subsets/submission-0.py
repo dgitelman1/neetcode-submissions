01d86b35a0a911efe72df9862512be5c98efe8e7
Daniel G <92356476+dgitelman1@users.noreply.github.com>
2026-09-02 18:39:01 -0400

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # for each number we have subsets that include that number and subsets that don't
+        output = [[]]
+        res = []
+        def helper(i):
+            # at i we can either include or disclude the current i
+            # then we can move onto the next i for each group
+            nonlocal res
+            nonlocal output
+            if i>=len(nums):
+                return
+            res.append(nums[i])
+            output.append(res.copy())
+            helper(i+1)
+            res.pop()
+            helper(i+1)
+        helper(0)
+        return output
